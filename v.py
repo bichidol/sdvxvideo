83916b142378a7aed72e22fe70d42830e5ee98e5
kari <125827646+bichidol@users.noreply.github.com>
2023-09-06 23:48:18 -0700

    remove youtube

--- v.py
+++ v.py
@@ -9,33 +9,14 @@ import xml.etree.ElementTree as ET
 import cv2
 import shutil
 import re
-import html
-from html import unescape
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-from googleapiclient.errors import HttpError
-#from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.support.ui import WebDriverWait
-#from selenium.webdriver.support import expected_conditions as EC
-
 
 parser = argparse.ArgumentParser(description='Create a video from jacket and audio.')
 parser.add_argument('folderpaths', type=str, nargs='*', help='Paths to folders containing the audio and image files')
 parser.add_argument('--musicdb', '-m', type=str, default='music_db.xml', help='Path to musicdb file')
 parser.add_argument('--rootfolder', '-r', type=str, help='Path to root folder containing all song folders')
 parser.add_argument('--outputdir', '-o', type=str, default=None, help='Path to output directory (optional)')
-parser.add_argument('--upload', '-u', action='store_true', help='Upload to youtube or not')
-parser.add_argument('--client_json', '-c', type=str, default='client.json', help='Path to client_secrets.json file')
 
 args = parser.parse_args()
-
-def get_youtube_service(client_json_path):
-    flow = InstalledAppFlow.from_client_secrets_file(client_json_path, ['https://www.googleapis.com/auth/youtube.upload'])
-    credentials = flow.run_local_server(port=0)
-    youtube = build('youtube', 'v3', credentials=credentials)
-    return youtube
 
 if args.rootfolder:
     args.folderpaths = []
@@ -53,31 +34,15 @@ def get_music_info(music_id, music_db_path):
         root = ET.fromstring(contents)
         music = root.find(f"./music[@id='{int(music_id):d}']")
         if music:
-            title_name = unescape(music.find('./info/title_name').text)
+            title_name = music.find('./info/title_name').text
             artist_name = music.find('./info/artist_name').text
-            version = int(music.find('./info/version').text)
-            
-            version_dict = {
-                1: "BOOTH",
-                2: "INFINITE INFECTION",
-                3: "GRAVITY WARS",
-                4: "HEAVENLY HAVEN",
-                5: "VIVID WAVE",
-                6: "EXCEED GEAR",
-                7: "???",
-                8: "???"
-            }
-            
-            version_title = version_dict.get(version, f"Version {version}")
-
-            return title_name, artist_name, version_title
+            return title_name, artist_name
         else:
-            print(f"Song ID {music_id} not found in musicdb.")
-            return None, None, None
+            print(f"Song ID {music_id} not found in the music database.")
+            return None, None
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None, None, None
-
+        return None, None
 
 
 def get_sanitized_filename(filename):
@@ -120,14 +85,14 @@ def get_sanitized_filename(filename):
     for bad, good in homoglyphs.items():
         filename = filename.replace(bad, good)
     return filename
-    
-    
-def create_video(folderpath, music_db_path, output_directory, youtube):
+
+
+def create_video(folderpath, music_db_path, output_directory):
     music_id = os.path.basename(folderpath)[:4]
 
-    title_name, artist_name, version_title = get_music_info(music_id, music_db_path)
+    title_name, artist_name = get_music_info(music_id, music_db_path)
 
-    if not title_name or not artist_name or not version_title: 
+    if not title_name or not artist_name:
         return
 
     video_title = f"{artist_name} - {title_name}"
@@ -181,39 +146,6 @@ def create_video(folderpath, music_db_path, output_directory, youtube):
         output_file_name = os.path.join(output_directory, video_title + ".mp4")
 
     video.write_videofile(output_file_name, audio_codec="aac")
-    
-    if youtube:
-        try:
-            request = youtube.videos().insert(
-                part="snippet,status",
-                body={
-                    "snippet": {
-                        "title": video_title,
-                        "description": f"Composed by {artist_name}, from SDVX {version_title}\n\nSOUND VOLTEX\nhttps://p.eagate.573.jp/game/sdvx/\n© Konami Amusement",
-                        "tags": ["sdvx"],
-                        "categoryId": "10"
-                    },
-                    "status": {
-                        "privacyStatus": "unlisted",
-                        "selfDeclaredMadeForKids": False 
-                    }
-                },
-                media_body=MediaFileUpload(output_file_name)
-            )
-            response = request.execute()
-            print(f"Video '{video_title}' upload done: https://www.youtube.com/watch?v={response['id']}") 
-        except HttpError as e:
-            print(f"Error occurred: {e}")
 
-def main():
-    args = parser.parse_args()
-    
-    youtube_service = None
-    if args.upload:
-        youtube_service = get_youtube_service(args.client_json)
-        
-    for folderpath in args.folderpaths:
-        create_video(folderpath, args.musicdb, args.outputdir, youtube_service)
-        
-if __name__ == "__main__":
-    main()
+for folderpath in args.folderpaths:
+    create_video(folderpath, args.musicdb, args.outputdir)
